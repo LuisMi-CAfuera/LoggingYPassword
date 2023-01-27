82eb0d1bb2a7cf369a9ca2228d95d1c3f4e27e9a
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2023-01-27 11:08:11 +0100

    2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -10,6 +11,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var login = ""
+        var pass = ""
+        binding.Login.isEnabled = false
+
+        if(binding.UsuarioEdit.text.toString().equals(binding.PassEdit.text.toString())){
+            binding.Login.isEnabled = true
+        }
+
+        binding.Login.setOnClickListener {
+            login = binding.UsuarioEdit.text.toString()
+            pass = binding.PassEdit.text.toString()
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            intent.putExtra("LOGIN", login)
+            intent.putExtra("PASS", pass)
+            startActivity(intent)
+        }
+
+        binding.Registro.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
 
 
 
