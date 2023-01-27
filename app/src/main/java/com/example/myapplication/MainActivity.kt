d37f80bba75b34c7e3425cae83e3d2433bc8f720
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2023-01-27 13:25:54 +0100

    5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.Toast
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -12,27 +16,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var login = ""
-        var pass = ""
-        binding.Login.isEnabled = false
+        var login = intent.getStringExtra("LOGIN")
+        var pass = intent.getStringExtra("PASS")
+        //si usurio y contraseña estan vacios no se puede iniciar sesion
 
-        if(binding.UsuarioEdit.text.toString().equals(binding.PassEdit.text.toString())){
-            binding.Login.isEnabled = true
-        }else{
-            Toast.makeText(getApplicationContext(), "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-        }
+
+        binding.PassEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+
+            }
+            override fun afterTextChanged(s: Editable?) {
+                if ((login.isNullOrEmpty() && pass.isNullOrEmpty()) && !binding.UsuarioEdit.text.toString().equals(binding.PassEdit.text.toString())) {
+                    Toast.makeText(this@MainActivity, "Usuario y contraseña incorrectos o no te has registrado", Toast.LENGTH_SHORT).show()
+                    binding.Login.isEnabled = false
+                } else if(!(login.isNullOrEmpty() && pass.isNullOrEmpty()) && binding.UsuarioEdit.text.toString().equals(binding.PassEdit.text.toString())){
+                    binding.Login.isEnabled = true
+                }
+            }
+        })
+
+
+
+
+
+
 
         binding.Login.setOnClickListener {
             login = binding.UsuarioEdit.text.toString()
             pass = binding.PassEdit.text.toString()
-            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            val intent = Intent(this@MainActivity, MainActivity3::class.java)
             intent.putExtra("LOGIN", login)
             intent.putExtra("PASS", pass)
             startActivity(intent)
         }
 
         binding.Registro.setOnClickListener {
-            val intent = Intent(this@MainActivity, MainActivity3::class.java)
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/myapplication/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -1,11 +1,29 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMain2Binding
+
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var login = ""
+        var pass = ""
+
+        login = binding.NombreEdit.text.toString()
+        pass = binding.ContraseAEdit2.text.toString()
+
+        binding.Registrar.setOnClickListener{
+            val intent = Intent(this@MainActivity2, MainActivity::class.java)
+            intent.putExtra("LOGIN", login)
+            intent.putExtra("PASS", pass)
+            startActivity(intent)
+        }
     }
 }
